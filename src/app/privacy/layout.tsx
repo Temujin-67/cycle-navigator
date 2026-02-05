4c78702c1a7d3d8fef936b4a95bae4b236a42a10
Temujin-67 <67160066+Temujin-67@users.noreply.github.com>
2026-02-05 14:52:23 +0200

    Create layout.tsx

---
+++ src/app/privacy/layout.tsx
@@ -0,0 +1,13 @@
+export default function PrivacyLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html>
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+}
